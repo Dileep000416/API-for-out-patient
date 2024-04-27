f328802e76b4d151ce4409967d24368960493dd6
Dileep Kumar <janupaladileep419@gmail.com>
2024-04-27 12:09:53 +0530

    initial commit

--- appointment_system/appointments/views.py
+++ appointment_system/appointments/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from rest_framework import viewsets
 from .models import Doctor, Availability, Appointment
 from .serializers import DoctorSerializer, AvailabilitySerializer, AppointmentSerializer
@@ -15,3 +14,13 @@ class AvailabilityViewSet(viewsets.ModelViewSet):
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
+# Add the doctor_detail view function here
+from django.shortcuts import render, get_object_or_404
+from .models import Doctor
+
+def doctor_detail(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    return render(request, 'doctors.html', {'doctor': doctor})
+
+
